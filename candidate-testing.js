957cfdc19d4b1b229b55ec04231f8f5ff532cac3
Riasat <hazara_journal@me.com>
2021-06-27 20:55:55 +0000

    Part 2 completed with some part 3 in progress in this push.

--- candidate-testing.js
+++ candidate-testing.js
@@ -1,37 +1,44 @@
 const input = require('readline-sync');
 
-// TODO 2: modify your quiz app to ask 5 questions //
-
 // TODO 1.1a: Define candidateName // 
 let candidateName;
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 let question;
 let correctAnswer = "Sally Ride";
 let candidateAnswer;
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions = [
+  "Who was the first American woman in space? ", 
+  "True or false: 5 kilometer == 5000 meters? ", 
+  "(5 + 3)/2 * 10 = ? ", 
+  "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS?	"];
+let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
+let candidateAnswers = [];
 
 
 function askForName() {
-  // TODO 1.1b: Ask for candidate's name //
+  // Prompt question ask for candidate's name //
   candidateName = input.question("Hello! Can I have your name please? ")
+
 }
 
 function askQuestion() {
-  // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-  candidateAnswer = input.question("Who was the first American woman in space? ")
+  // Loop through candidateAnswers array[i] and prompt questions from questions array[i]
+  for (i = 0; i < questions.length; i++) {
+    //Prompt quiz questions and save it in candidateAnswers array
+    candidateAnswers[i] = input.question(`${i + 1}) ${questions[i]}`);
+    
+    //Provide feedback for each question
+    console.log(
+      `Your answer: ${candidateAnswers[i]}
+      Correct answer: ${correctAnswers[i]}
+      `);
+  }
 }
 
 function gradeQuiz(candidateAnswers) {
-
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
 
-  let grade;
-
-  grade = (candidateAnswer === correctAnswer) ? "Great! Thats right, she was the first American women in space" : "That doesn't seem right, try again! ";
-
-  console.log(grade);
+  let grade = (candidateAnswer === correctAnswer) ? "Great! Thats right, she was the first American women in space" : "That doesn't seem right, try again! ";
 
   console.log(grade);
   return grade;
@@ -41,7 +48,6 @@ function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
   console.log("Welcome " + candidateName)
-  
   
   askQuestion();
   gradeQuiz(this.candidateAnswers);
